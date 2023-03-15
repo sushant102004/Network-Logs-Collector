0c9115858f35c3bb720aaa4fc61c2c9637961a93
sushant102004 <sushant.dhiman9812@gmail.com>
2023-03-15 23:41:58 +0530

    support to capture logs for different page *static

--- logs-capture/app.py
+++ logs-capture/app.py
@@ -7,13 +7,40 @@ import json
 
 app = FastAPI()
 
+def saveLogs(driver):
+    logs = driver.get_log("performance")
+
+    with open('network_log.json', 'a', encoding='utf-8') as f:
+        for log in logs:
+            network_log = json.loads(log['message'])['message']
+
+            if('Network.response' in network_log['method']):
+
+                if network_log['method'] == 'Network.responseReceived':
+                    mimeType = network_log['params']['response']['mimeType']
+                    log_name = network_log['params']['response']['url']
+                    status_code = network_log['params']['response']['status']
+
+                    data = {'log_name': log_name, 'status_code': status_code, 'type': mimeType}
+                    f.write(json.dumps(data) + ',')
+
+        f.write('{}]')
+
+        print('Logs Saved')
+
+
+
 def captureNetworkLogs(site: str):
+    # Clear network_log.json
+    open('network_log.json', 'w').close()
+
     desired_capabilities = DesiredCapabilities.CHROME
     desired_capabilities['goog:loggingPrefs'] = {'performance': 'ALL'}
 
     options = webdriver.ChromeOptions()
+    options.add_argument('--window-size=1920,1080')
 
-    options.add_argument('headless')
+    # options.add_argument('headless')
 
     options.add_argument('--ignore-certificate-error')
 
@@ -23,30 +50,14 @@ def captureNetworkLogs(site: str):
 
     time.sleep(10)
 
-    def saveLogs():
-        logs = driver.get_log("performance")
+    # Saving logs for intial page load
+    saveLogs(driver)
 
-        with open('network_log.json', 'w', encoding='utf-8') as f:
-            f.write("[")
-
-            for log in logs:
-                network_log = json.loads(log['message'])['message']
-
-                if('Network.response' in network_log['method']):
-
-                    if network_log['method'] == 'Network.responseReceived':
-                        mimeType = network_log['params']['response']['mimeType']
-                        log_name = network_log['params']['response']['url']
-                        status_code = network_log['params']['response']['status']
-
-                        data = {'log_name': log_name, 'status_code': status_code, 'type': mimeType}
-                        f.write(json.dumps(data) + ',')
-
-            f.write('{}]')
-
-        print('Logs Saved')
-
-        saveLogs()
+    menuLink = driver.find_element(by = By.LINK_TEXT, value = 'Sign up for Free')
+    menuLink.click()
+    time.sleep(10)
+    # Logs for Enterprise page
+    saveLogs(driver)
 
     driver.quit()
 
