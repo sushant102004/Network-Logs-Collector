d9da41947581c0c1f5908ad9d0a15d4b018fa5ee
sushant102004 <sushant.dhiman9812@gmail.com>
2023-03-17 23:40:44 +0530

    ✨ feat(SearchPage.tsx): add support for scanning multiple pages by link text (UI)

--- app/frontend/src/components/SearchPage.tsx
+++ app/frontend/src/components/SearchPage.tsx
@@ -7,7 +7,7 @@ export default function SearchPage() {
 
     return (
         <>
-            <div className = 'text-center mt-20'>
+            <div className = 'text-center mt-14'>
                 <p className = 'text-slate-800 text-2xl '>Website</p>
                 <p className = 'mt-4 text-slate-700'>Enter a website URL in below input field. Make sure to only enter domain name. <br/>Do not enter any subdomain or protocol.</p>
                 <input
@@ -16,6 +16,22 @@ export default function SearchPage() {
                 value={site}
                 onChange={(event) => setSite(event.target.value)}
                 ></input>
+
+
+
+                {/* Pages To Scan */}
+                <p className = 'text-slate-800 text-2xl mt-16'>Pages</p>
+                <p className = 'mt-4 text-slate-700'>Enter link text from above website if you want to scan those pages also. Make sure to only enter link text do not enter link itself. <br/>Example: "Start Scan" is a link text which will go to another page on click.</p>
+                <textarea
+                className="focus:outline-0 bg-slate-50 border-2 rounded-md w-96 h-36 p-2 mt-6"
+                placeholder = 'Enterprise&#10;Pricing&#10;Platform'
+                value={site}
+                onChange={(event) => setSite(event.target.value)}
+                ></textarea>
+
+                <div className="flex w-24 h-8 bg-yellow-400 rounded-md justify-center items-center text-sm text-slate-800 ml-auto mr-auto mt-6">
+                <a href="/" className="bg-transparent">Start Scan</a>
+            </div>
             </div>
         </>
     )
