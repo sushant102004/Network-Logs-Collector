d19d125a5d8c7767f612b6eed6c62fe9f74a13ae
sushant102004 <sushant.dhiman9812@gmail.com>
2023-03-17 23:50:25 +0530

    🎨 style(SearchPage.tsx): add responsive width to pages textarea

--- app/frontend/src/components/SearchPage.tsx
+++ app/frontend/src/components/SearchPage.tsx
@@ -23,7 +23,7 @@ export default function SearchPage() {
                 <p className = 'text-slate-800 text-2xl mt-16'>Pages</p>
                 <p className = 'mt-4 text-slate-700 text-sm ml-6 mr-6 lg:ml-0 lg:mr-0'>Enter link text from above website if you want to scan those pages also. Make sure to only enter link text do not enter link itself. <br/>Example: "Start Scan" is a link text which will go to another page on click.</p>
                 <textarea
-                className="focus:outline-0 bg-slate-50 border-2 rounded-md w-11/12 h-36 p-2 mt-6"
+                className="focus:outline-0 bg-slate-50 border-2 rounded-md w-11/12 h-36 p-2 mt-6 lg:w-96"
                 placeholder = 'Enterprise&#10;Pricing&#10;Platform'
                 value={pages}
                 onChange={(event) => setPages(event.target.value)}
